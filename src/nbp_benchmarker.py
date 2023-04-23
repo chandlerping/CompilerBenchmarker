8aee9ff164e4ad35cbdf08bec9054df781706420
chandlerping <18307130025@fudan.edu.cn>
2023-04-23 00:04:05 -0400

    modify shell

--- src/nbp_benchmarker.py
+++ src/nbp_benchmarker.py
@@ -32,7 +32,7 @@ class NBPBenchmarker(Benchmarker):
 
     def benchmark(self):
         os.chdir('NPB3.0-omp-C')
-        subprocess.run('mkdir bin')
+        subprocess.run('mkdir bin', shell=True)
         subprocess.run('make ' + self.test + ' ' + self.size, shell=True)
         os.chdir('bin')
         cmd = './' + self.test + '.' + self.size
