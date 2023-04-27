0dda3576678d5e0b583cc9f360e83d20e26b8a38
chandlerping <18307130025@fudan.edu.cn>
2023-04-27 15:50:33 -0400

    modify path

--- src/benchmarker/nbp_benchmarker.py
+++ src/benchmarker/nbp_benchmarker.py
@@ -48,7 +48,7 @@ class NBPBenchmarker(Benchmarker):
         cmd = './' + self.test + '.' + self.size
         print("RUNNING PROGRAM AND TIMING FOR "+ str(compiler) + " " + str(self.test) + " " + str(self.size) + " " + str(self.group))
         self.output = subprocess.getoutput(cmd).split('\n')
-        os.chdir('../../..')
+        os.chdir('../..')
 
     def report(self):
         os.chdir('NPB3.0-omp-C')
