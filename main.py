36bfe0bb01266c8e049b3203de565ce39c8a9efb
chandlerping <18307130025@fudan.edu.cn>
2023-04-27 13:08:39 -0400

    modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.nbp_benchmarker import *
+from src.tsvc_benchmarker import *
 
 
 def main():
     nbp_b = NBPBenchmarker()
     nbp_b.set_test('ft', 'S', 'li')
+
+    tsvc_b = TSVCBenchmarker()
+    tsvc_b.set_test('novec', 'default', 'lu')
 
 if __name__ == '__main__':
     main()
