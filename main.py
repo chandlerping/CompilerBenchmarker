e4aae7ba10744e373c88e8067a178b116842c2d5
chandlerping <18307130025@fudan.edu.cn>
2023-04-27 14:36:58 -0400

    add comment

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from src.tsvc_benchmarker import *
 
 
 def main():
-    # nbp_b = NBPBenchmarker()
-    # nbp_b.set_test('ft', 'S', 'li')
+    nbp_b = NBPBenchmarker()
+    nbp_b.set_test('ft', 'S', 'li')
 
+    # only use the first 4 tests in TSVC_2 for time concern, can uncomment more tests in "./TSVC_2/src/tsvc.c"
     tsvc_b = TSVCBenchmarker()
     tsvc_b.set_test('novec', 'default', 'lu')
+
 
 if __name__ == '__main__':
     main()
